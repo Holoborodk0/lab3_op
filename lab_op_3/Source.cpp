eb70060816abd1f8c2aa9f4a888da4daf7fd6c76
Mykhailo <vatazkomisa@gmail.com>
2021-05-19 13:44:41 +0300

    add class ListNode

--- lab_op_3/Source.cpp
+++ lab_op_3/Source.cpp
@@ -3,6 +3,23 @@
 
 using namespace std;
 
+class ListNode
+{
+private:
+	int value;
+	int key;
+	ListNode* next;
+public:
+	ListNode(int key, int value);
+};
+
+ListNode::ListNode(int key, int value)
+{
+	this->key = key;
+	this->value = value;
+	this->next = nullptr;
+}
+
 int main()
 {
 
