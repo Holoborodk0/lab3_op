a5c1973e7fa19fd80b4cd7b7f0c8753723833b7c
Mykhailo <vatazkomisa@gmail.com>
2021-05-19 17:07:07 +0300

    add HashTable and func Insert

--- lab_op_3/Source.cpp
+++ lab_op_3/Source.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
 #include<fstream>
+#include<string>;
 
 using namespace std;
 
 class ListNode
 {
 private:
-	int value;
-	int key;
+	string value;
+	string key;
 public:
 	ListNode* next;
-	ListNode(int key, int value);
+	ListNode(string key, string value);
 };
 
-ListNode::ListNode(int key, int value)
+ListNode::ListNode(string key, string value)
 {
 	this->key = key;
 	this->value = value;
@@ -25,6 +26,18 @@ class HashTable
 private:
 	int size = 100;
 	ListNode** ptr;
+
+	int HashFunc(string key)
+	{
+		int sum = 0;
+		int size = key.size() * 2;
+		for (int i = 0; i < key.size(); i++)
+		{
+			sum += key[i];
+		}
+		int res = sum + size;
+		return res % this->size;
+	}
 public:
 
 	HashTable()
@@ -36,51 +49,58 @@ public:
 		}
 	}
 
-	void Check()
-	{
-		int count;
-		for (int i = 0; i < this->size; i++)
-		{
-			if (ptr[i] != nullptr) count++;
-			if (count > (int)0.8 * this->size) Resize();
-			else continue;
-		}
-		count = 0;
-	}
+	//void Check()
+	//{
+	//	int count;
+	//	for (int i = 0; i < this->size; i++)
+	//	{
+	//		if (ptr[i] != nullptr) count++;
+	//		if (count > (int)0.8 * this->size) Resize();
+	//		else continue;
+	//	}
+	//	count = 0;
+	//}
 
-	void Resize()
-	{
-		this->size = this->size * 2;
-		ListNode** Newptr;
-		Newptr = new ListNode * [this->size];
-		for (int i = 0; i < size; i++)
-		{
-			Newptr[i] = nullptr;
-		}
+	//void Resize()
+	//{
+	//	this->size = this->size*2;
+	//	ListNode** Newptr;
+	//	Newptr = new ListNode * [this->size];
+	//	for (int i = 0; i < size; i++)
+	//	{
+	//		Newptr[i] = nullptr;
+	//	}
 
-	}
-
-	int HashFunc(string key)
-	{
-		int sum = 0;
-		int size = key.size() * 2;
-		for (int i = 0; i < key.size(); i++)
-		{
-			key[i] += sum;
-		}
-		int res = sum + size;
-		return res % this->size;
-	}
+	//}
 
 	void Insert(string key, string value)
 	{
 		int hash = HashFunc(key);
 		ListNode* curr_index = this->ptr[hash];
-		ListNode* prev = nullptr;
+		ListNode* previosly = nullptr;
 		while (curr_index != nullptr)
 		{
-			prev = curr_index;
+			previosly = curr_index;
 			curr_index = curr_index->next;
+		}
+
+		if (curr_index == nullptr)
+		{
+			curr_index = new ListNode(key, value);
+			if (previosly == nullptr)
+			{
+				this->ptr[hash] = curr_index;
+			}
+			else
+			{
+				ListNode* temp1 = this->ptr[hash];
+				while (temp1->next != nullptr)
+				{
+					temp1 = temp1->next;
+				}
+				temp1->next = curr_index;
+			}
+			int b;
 		}
 
 	}
@@ -89,5 +109,10 @@ public:
 
 int main()
 {
-
+	HashTable table;
+	table.Insert("A", "ff");
+	table.Insert("Bd", "fxa");
+	table.Insert("A", "sgrgc");
+	table.Insert("A", "segkgkx");
+	table.Insert("A", "segffdffgfgrfgekgkx");
 }
